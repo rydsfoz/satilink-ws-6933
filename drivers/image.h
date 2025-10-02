5caf666a0d686f07ca2906eecadf7fef2d1943d8
Rosiney Desidério da Silva <ryds.foz@gmail.com>
2025-10-02 13:26:06 -0300

    Add "image".

---
+++ drivers/image.h
@@ -0,0 +1,28 @@
+// image (...)
+//
+// TODO: Explicacao/descricao (...)
+
+// This program is open source. For license terms, see the LICENSE file.
+// Licensed under the Academic Free License version 3.0.
+
+// Author: Rosiney D. da Silva, 2020-2024.
+
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <inttypes.h>
+
+extern const uint8_t image_menu_0[];  // menu: sat setting
+extern const uint8_t image_menu_1[];  // menu: signal scan
+extern const uint8_t image_menu_2[];  // menu: tools
+extern const uint8_t image_menu_3[];  // menu: sat manage
+extern const uint8_t image_menu_4[];  // menu: tp manager
+extern const uint8_t image_menu_5[];  // menu: system set
+extern const uint8_t image_tools_0[]; // tools: azimuth
+extern const uint8_t image_tools_1[]; // tools: elevation
+extern const uint8_t image_tools_2[]; // tools: polarization
+
+#endif // IMAGE_H
+
+// Reference(s):
+//   @1: A: B - C (D)
