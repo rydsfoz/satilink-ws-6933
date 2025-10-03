3094e08aa117ac369e0eb7f2a80bc9d15465985c
Rosiney Desidério da Silva <ryds.foz@gmail.com>
2025-10-02 23:19:07 -0300

    Add "ds3x0xx_firmware".

---
+++ drivers/ds3x0xx_firmware.h
@@ -0,0 +1,18 @@
+// M88DS3002B/M88DS3103/M88DS3103B firmware (...)
+//
+// TODO: Explicacao/descricao (...)
+
+// This program is open source. For license terms, see the LICENSE file.
+// Licensed under the Academic Free License version 3.0.
+
+// Author: Rosiney D. da Silva, 2020-2025.
+
+#ifndef DS3X0XX_FIRMWARE_H
+#define DS3X0XX_FIRMWARE_H
+
+#include <inttypes.h>
+
+extern const uint8_t ds3x0xx_firmware[];
+
+#endif // DS3X0XX_FIRMWARE_H
+
