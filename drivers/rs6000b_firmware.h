9ba529fe6bbf4e92fc3d1728954540a9c7a414a9
Rosiney Desidério da Silva <ryds.foz@gmail.com>
2025-10-02 23:18:16 -0300

    Add "rs6000b_firmware".

---
+++ drivers/rs6000b_firmware.h
@@ -0,0 +1,18 @@
+// M88RS6000B firmware (...)
+//
+// TODO: Explicacao/descricao (...)
+
+// This program is open source. For license terms, see the LICENSE file.
+// Licensed under the Academic Free License version 3.0.
+
+// Author: Rosiney D. da Silva, 2020-2025.
+
+#ifndef RS6000B_FIRMWARE_H
+#define RS6000B_FIRMWARE_H
+
+#include <inttypes.h>
+
+extern const uint8_t rs6000b_firmware[];
+
+#endif // RS6000B_FIRMWARE_H
+
