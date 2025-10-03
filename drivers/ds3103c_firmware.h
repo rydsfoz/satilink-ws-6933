f7ed4d02f50233f6205e5e6095c8cd13d5217a63
Rosiney Desidério da Silva <ryds.foz@gmail.com>
2025-10-02 23:19:48 -0300

    Add "ds3103c_firmware".

---
+++ drivers/ds3103c_firmware.h
@@ -0,0 +1,18 @@
+// M88DS3103C firmware (...)
+//
+// TODO: Explicacao/descricao (...)
+
+// This program is open source. For license terms, see the LICENSE file.
+// Licensed under the Academic Free License version 3.0.
+
+// Author: Rosiney D. da Silva, 2020-2025.
+
+#ifndef DS3103C_FIRMWARE_H
+#define DS3103C_FIRMWARE_H
+
+#include <inttypes.h>
+
+extern const uint8_t ds3103c_firmware[];
+
+#endif // DS3103C_FIRMWARE_H
+
