74945ecf7b677416cc7f62ac40c1453d5a72b1ea
Rosiney Desidério da Silva <ryds.foz@gmail.com>
2025-10-08 16:30:09 -0300

    Add "font_data".

---
+++ drivers/font_data.h
@@ -0,0 +1,30 @@
+// font_data (...)
+//
+// TODO: Explicacao/descricao (...)
+
+// This program is open source. For license terms, see the LICENSE file.
+// Licensed under the Academic Free License version 3.0.
+
+// Author: Rosiney D. da Silva, 2020-2025.
+
+#ifndef FONT_DATA_H
+#define FONT_DATA_H
+
+#include <inttypes.h>
+
+extern const uint8_t font_8x16[];
+extern const uint8_t font_24x32[];
+extern const uint8_t font_lower_width[];
+extern const uint8_t font_lower[];
+extern const uint8_t font_upper_width_g0[];
+extern const uint8_t font_upper_g0[];
+extern const uint8_t font_upper_width_g1[];
+extern const uint8_t font_upper_g1[];
+extern const uint8_t font_upper_width_g2[];
+extern const uint8_t font_upper_g2[];
+extern const uint8_t font_upper_width_g3[];
+extern const uint8_t font_upper_g3[];
+extern const uint8_t font_upper_width_g4[];
+extern const uint8_t font_upper_g4[];
+
+#endif // FONT_DATA_H
